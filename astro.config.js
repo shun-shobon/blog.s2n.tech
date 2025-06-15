80e450bcf1e1136b59817a71a533d88a97a374fe
NISHIZAWA Shuntaro <me@s2n.tech>
2025-06-15 11:22:50 +0900

    feat: Tailwindを追加

--- astro.config.js
+++ astro.config.js
@@ -1,3 +1,8 @@
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
-export default defineConfig({});
+export default defineConfig({
+	vite: {
+		plugins: [tailwindcss()],
+	},
+});
