0700ff9907b89f8694b44c3e782eefd2d00a6b2d
NISHIZAWA Shuntaro <me@s2n.tech>
2025-07-17 09:44:00 +0900

    feat: Tailwindの設定を追加

--- astro.config.js
+++ astro.config.js
@@ -1,10 +1,23 @@
 import mdx from "@astrojs/mdx";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 export default defineConfig({
 	vite: {
 		plugins: [tailwindcss()],
 	},
 	integrations: [mdx()],
+	experimental: {
+		fonts: [
+			{
+				provider: fontProviders.google(),
+				name: "Caveat",
+				cssVariable: "--font-family-caveat",
+				weights: [400, 700],
+				subsets: ["latin"],
+				styles: ["normal"],
+				fallbacks: [],
+			},
+		],
+	},
 });
