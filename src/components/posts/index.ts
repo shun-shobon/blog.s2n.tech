2b13ecc122ab0c569c6bbb88f13c846b7ad8f360
NISHIZAWA Shuntaro <me@s2n.tech>
2025-07-21 13:16:43 +0900

    feat: ツイートを追加

--- src/components/posts/index.ts
+++ src/components/posts/index.ts
@@ -1,3 +1,4 @@
 export { default as Aside } from "./Aside.astro";
 export { default as Details } from "./Details.astro";
 export { default as LinkCard } from "./LinkCard.astro";
+export { default as Tweet } from "./Tweet.astro";
