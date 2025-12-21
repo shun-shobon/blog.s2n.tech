2a5209e800433f32d233838f58f3af679dd2a4b3
NISHIZAWA Shuntaro <me@s2n.tech>
2025-12-21 09:08:13 +0900

    feat: ページ取得をキャッシング

--- src/pages/api/open-graph/index.ts
+++ src/pages/api/open-graph/index.ts
@@ -1,6 +1,8 @@
 import type { APIRoute } from "astro";
 import { normalizeURL } from "ufo";
 
+import { logger } from "@/libs/logger";
+
 import { extractOpenGraph } from "./_internal/extract";
 
 export const prerender = false;
@@ -8,7 +10,7 @@ export const prerender = false;
 const CACHE_S_MAX_AGE = 60 * 60 * 24 * 7; // 1週間
 const CACHE_MAX_AGE = 60 * 60; // 1時間
 
-export const GET: APIRoute = async ({ request }) => {
+export const GET: APIRoute = async ({ request, locals }) => {
 	const searchParams = new URL(request.url).searchParams;
 	const targetURL = searchParams.get("url");
 	if (!targetURL) {
@@ -17,7 +19,7 @@ export const GET: APIRoute = async ({ request }) => {
 
 	const normalizedTargetURL = normalizeURL(targetURL);
 	const targetRequest = new Request(normalizedTargetURL);
-	const targetResponse = await fetch(targetRequest);
+	const targetResponse = await cachedFetch(targetRequest, locals);
 	if (!targetResponse.ok) {
 		return new Response("Not Found", { status: 404 });
 	}
@@ -31,40 +33,40 @@ export const GET: APIRoute = async ({ request }) => {
 	});
 };
 
-// // 何故かworker-configuration.d.tsのCacheStorageが読み込まれないため、型を追加
-// declare global {
-// 	interface CacheStorage {
-// 		default: CacheStorage;
-// 		put(request: Request, response: Response): Promise<void>;
-// 	}
-// }
+// 何故かworker-configuration.d.tsのCacheStorageが読み込まれないため、型を追加
+declare global {
+	interface CacheStorage {
+		default: CacheStorage;
+		put(request: Request, response: Response): Promise<void>;
+	}
+}
 
-// async function cachedFetch(
-// 	request: Request,
-// 	locals: App.Locals,
-// ): Promise<Response> {
-// 	// 開発環境ではキャッシュが使えないので、fetchをそのまま返す
-// 	if (import.meta.env.DEV) {
-// 		return await fetch(request);
-// 	}
+async function cachedFetch(
+	request: Request,
+	locals: App.Locals,
+): Promise<Response> {
+	// 開発環境ではキャッシュが使えないので、fetchをそのまま返す
+	if (import.meta.env.DEV) {
+		return await fetch(request);
+	}
 
-// 	const cachedResponse = await caches.default.match(request);
-// 	if (cachedResponse) {
-// 		logger.info("cache hit", { url: request.url });
-// 		return cachedResponse;
-// 	}
+	const cachedResponse = await caches.default.match(request);
+	if (cachedResponse) {
+		logger.info("cache hit", { url: request.url });
+		return cachedResponse;
+	}
 
-// 	const response = await fetch(request);
-// 	// 失敗の場合はキャッシュしない
-// 	if (!response.ok) {
-// 		return response;
-// 	}
+	const response = await fetch(request);
+	// 失敗の場合はキャッシュしない
+	if (!response.ok) {
+		return response;
+	}
 
-// 	// レスポンスヘッダーを書き換えて、強制的にキャッシュを行う
-// 	const clonedResponse = response.clone();
-// 	const newResponse = new Response(clonedResponse.body, clonedResponse);
-// 	newResponse.headers.set("Cache-Control", `public, s-maxage=${CACHE_MAX_AGE}`);
-// 	locals.runtime.ctx.waitUntil(caches.default.put(request, newResponse));
+	// レスポンスヘッダーを書き換えて、強制的にキャッシュを行う
+	const clonedResponse = response.clone();
+	const newResponse = new Response(clonedResponse.body, clonedResponse);
+	newResponse.headers.set("Cache-Control", `public, s-maxage=${CACHE_MAX_AGE}`);
+	locals.runtime.ctx.waitUntil(caches.default.put(request, newResponse));
 
-// 	return response;
-// }
+	return response;
+}
