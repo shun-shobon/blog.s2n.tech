823513e2dee02a2ad5ae8aafe8023f1134c9082b
NISHIZAWA Shuntaro <me@s2n.tech>
2025-12-26 16:02:53 +0900

    feat: OG画像も変更

--- src/libs/og-image.tsx
+++ src/libs/og-image.tsx
@@ -15,13 +15,14 @@ const AUTHOR_NAME = "しゅん🌙";
 const WIDTH = 1200;
 const HEIGHT = 630;
 
-const COLOR_BORDER_PRIMARY = "#f5f5f5";
-// const COLOR_BORDER_PRIMARY = "#e5e5e5";
+// const COLOR_BORDER_PRIMARY = "#f5f5f5";
+const COLOR_BORDER_PRIMARY = "#e5e5e5";
 const COLOR_BACKGROUND_PRIMARY = "#ffffff";
-const COLOR_BACKGROUND_SECONDARY = "#f5f5f5";
 const COLOR_TEXT_PRIMARY = "#262626";
+const COLOR_TEXT_SECONDARY = "#525252";
 const COLOR_TEXT_HEADING = "#0a0a0a";
 const FONT_CAVEAT = "Caveat";
+const FONT_COURIER_PRIME = "Courier Prime";
 const FONT_NOTO_SANS_JP = "Noto Sans JP";
 const BACKGROUND_GRID_SIZE = "16px";
 
@@ -33,6 +34,9 @@ try {
 
 const fontNotoSansJP = await fs.readFile("./src/assets/NotoSansJP-Bold.ttf");
 const fontCaveat = await fs.readFile("./src/assets/Caveat-Bold.ttf");
+const fontCourierPrime = await fs.readFile(
+	"./src/assets/CourierPrime-Regular.ttf",
+);
 
 // ZWJ: Zero Width Joiner
 const ZWJ = String.fromCodePoint(0x20_0d);
@@ -132,15 +136,44 @@ function OGImage({ title, tags }: Props): ReactNode {
 								<span
 									key={tag}
 									style={{
-										backgroundColor: COLOR_BACKGROUND_SECONDARY,
-										padding: "2px 20px 2px 16px",
+										backgroundColor: COLOR_BACKGROUND_PRIMARY,
+										border: `1px solid ${COLOR_TEXT_SECONDARY}`,
+										color: COLOR_TEXT_SECONDARY,
+										padding: "4px 16px 4px 16px",
 										borderRadius: "9999px",
-										fontFamily: FONT_CAVEAT,
+										fontFamily: FONT_COURIER_PRIME,
 										fontSize: "28px",
-										fontWeight: "bold",
+										display: "flex",
+										alignItems: "center",
+										gap: "4px",
 									}}
 								>
-									#{tag}
+									<svg width="20" height="20" viewBox="0 0 24 24">
+										<g
+											fill="none"
+											stroke="currentColor"
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											stroke-width="2"
+										>
+											<path d="M12.586 2.586A2 2 0 0 0 11.172 2H4a2 2 0 0 0-2 2v7.172a2 2 0 0 0 .586 1.414l8.704 8.704a2.426 2.426 0 0 0 3.42 0l6.58-6.58a2.426 2.426 0 0 0 0-3.42z"></path>
+											<circle
+												cx="7.5"
+												cy="7.5"
+												r=".5"
+												fill="currentColor"
+											></circle>
+										</g>
+									</svg>
+									<span
+										style={{
+											fontSize: "24px",
+											lineHeight: "1",
+											marginTop: "2px",
+										}}
+									>
+										{tag}
+									</span>
 								</span>
 							))}
 						</div>
@@ -241,6 +274,11 @@ export async function generateImage({
 				name: FONT_CAVEAT,
 				data: fontCaveat,
 				weight: 700,
+			},
+			{
+				name: FONT_COURIER_PRIME,
+				data: fontCourierPrime,
+				weight: 400,
 			},
 		],
 		async loadAdditionalAsset(code, segment) {
